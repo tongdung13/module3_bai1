892e8e8dec8c9d3ea698009175ada1199548a1e8
tongdung13 <tongvanthienvu@gmail.com>
2020-12-14 14:25:33 +0700

    ok

---
+++ login/resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <form method="post">
+        <p>@csrf</p>
+        <p><input type="text" name="username" placeholder="username"></p>
+        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="submit" value="Dang nhap"></p>
+    </form>
+</body>
+</html>
+
---
+++ login/resources/views/login_error.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login_error</title>
+</head>
+<body>
+<h1>dang nhap that bat moi nhap lai</h1>
+</body>
+</html>
